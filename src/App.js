633194df806b382ebf8238fd9e9f690d0ccb13e4
Ojea <rnjsdhwo1325@gmail.com>
2022-05-31 00:12:09 +0900

    useEffect localStorage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    //useEffect 라는 Hook 을 사용하여 컴포넌트가 마운트 됐을 때 (처음 나타났을 때),
+    //언마운트 됐을 때 (사라질 때), 그리고 업데이트 될 때 (특정 props가 바뀔 때) 특정 작업을 처리하는 방법
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    //localStorage에 저장하고 유효한지 검증함
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+      //유효한지 검증하고 상태변경 해줌
+    }
+  }, []);
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
